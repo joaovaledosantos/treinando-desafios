24cbd0403c8053b93c629f3fc46fc13bf8da577b
João V. Santos <joaovaledosantos@gmail.com>
2024-03-08 11:25:21 -0300

    Qual a cor das 3 canetas de Joao?

---
+++ src/TresCanetasDeCoresDiferentes.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+//Joao tem 3 canetas de cores diferetes, azul, preta e vermelha
+//se eu responder qualquer outra cor diferente, no console mostrara que ERREI.
+public class TresCanetasDeCoresDiferentes {
+    public static void main(String[] args) {
+        System.out.println("Qual a cor das 3 canetas de Joao?");
+        Scanner leitor = new Scanner(System.in);
+        String resposta = leitor.nextLine();
+        System.out.println("A cor da caneta que voce respondeu foi: " + resposta);
+
+        if (resposta.equals("azul") || resposta.equals("preta") || resposta.equals("vermelha")){
+            System.out.println("Voce acertou :)!");
+        }else{
+            System.out.println("Voce errou :(!");
+        }
+    }
+}
