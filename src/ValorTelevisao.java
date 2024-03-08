264be7d1cbea74bc83e078d1254bc31f2c9f877b
João V. Santos <joaovaledosantos@gmail.com>
2024-03-08 08:43:46 -0300

    valor da televisão

---
+++ src/ValorTelevisao.java
@@ -0,0 +1,13 @@
+//Se a televisao for = ou menor que 2.000 , eu compro
+//Se a televisao for main que 2.000, eu njao compro
+
+public class ValorTelevisao {
+    public static void main(String[] args) {
+        int televisao = 3000;
+        if (televisao <= 2000) {
+            System.out.println("Se a tv e menor ou igual a 2.000 ,eu compro!");
+        } else {
+            System.out.println("Se a tv e maior que 2.000 ,eu nao compro!");
+        }
+    }
+}
