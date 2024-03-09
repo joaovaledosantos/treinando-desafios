ace3a9d78654fae7a2a2e78d7b03b6e4d4991e4d
João V. Santos <joaovaledosantos@gmail.com>
2024-03-09 01:58:40 -0300

    Refaturação do desafio das tres canetas

--- src/TresCanetasDeCoresDiferentes.java
+++ src/TresCanetasDeCoresDiferentes.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 //Joao tem 3 canetas de cores diferetes, azul, preta e vermelha
 //se eu responder qualquer outra cor diferente, no console mostrara que ERREI.
 public class TresCanetasDeCoresDiferentes {
@@ -6,11 +7,14 @@ public class TresCanetasDeCoresDiferentes {
         System.out.println("Qual a cor das 3 canetas de Joao?");
         Scanner leitor = new Scanner(System.in);
         String resposta = leitor.nextLine();
+        resposta = resposta.toLowerCase().trim();
         System.out.println("A cor da caneta que voce respondeu foi: " + resposta);
 
-        if (resposta.equals("azul") || resposta.equals("preta") || resposta.equals("vermelha")){
+        if (resposta.equals("")) {
+            System.out.println("E obrigatorio informar uma cor!");
+        } else if (resposta.equals("azul") || resposta.equals("preta") || resposta.equals("vermelha")) {
             System.out.println("Voce acertou :)!");
-        }else{
+        } else {
             System.out.println("Voce errou :(!");
         }
     }
